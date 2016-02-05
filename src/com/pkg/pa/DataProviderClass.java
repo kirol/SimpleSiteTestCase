fbe3891c4405198a8acfd67db2b0a8007b111cec
minhqlrt <minh.lq7@gmail.com>
2016-02-05 13:56:53 +0700

    abc

--- src/com/pkg/pa/DataProviderClass.java
+++ src/com/pkg/pa/DataProviderClass.java
@@ -25,17 +25,21 @@ public class DataProviderClass {
 		FileInputStream in = new FileInputStream("D:\\eclipseee\\workplace\\SimpleSiteTestCase\\testdata.xls");
 		Workbook workbook = new HSSFWorkbook(in);
 		Sheet sheet = workbook.getSheet("Sheet1");
+		// getLastRowNum returns a number with a count starts from 0
 		Object[][] ob = new Object[sheet.getLastRowNum()][5];
 		int i=0;
 		for(int excelRow=1;excelRow<sheet.getLastRowNum()+1;excelRow++){
 			Row row = sheet.getRow(excelRow); 
+			System.out.println(row.getLastCellNum());
 			int j=0;
+			// getLastCellNum returns a number with a count starts from 1
 			for(int excelCell=0;excelCell<row.getLastCellNum();excelCell++){
 				ob[i][j] = row.getCell(excelCell).getStringCellValue();
 				j++;
 			}
 			i++;
 		}
+	    
 		workbook.close();
 		return ob;
 	}
--- src/com/pkg/test/LogInTest.java
+++ src/com/pkg/test/LogInTest.java
@@ -1,12 +1,7 @@
 package com.pkg.test;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
